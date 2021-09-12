743a9e70bd9f4973451a97e8353a33c5c33a4f31
cariocaphil <cariocaphil@gmail.com>
2021-09-12 18:09:42 +0200

    add basic game version

---
+++ pages/components/Game.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import Card from './Card';
+import { Modal } from 'antd';
+import { shuffleArray } from "../utils/utils";
+
+function Game({
+  options,
+  letterToBeDisplayed,
+  showFontInfo,
+  alwaysDifferentLetter,
+  fonts,
+  backgroundColor,
+  letters,
+  handleStartOver
+}) {
+  const [game, setGame] = useState([]);
+  const [flipCount, setFlipCount] = useState(0);
+  const [indexesOfFlippedCards, setIndexesOfFlippedCards] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    const newGame = [];
+    for (let i = 0; i < options / 2; i++) {
+      const optionA = {
+        id: i,
+        fontId: "font" + i,
+        flipped: false,
+        font: fonts && fonts[i]
+      }
+      const optionB = {
+        id: i + 1,
+        fontId: "font" + i,
+        flipped: false,
+        font: fonts && fonts[i]
+      }
+
+      newGame.push(optionA);
+      newGame.push(optionB);
+    }
+
+    const shuffledGame = shuffleArray(newGame);
+    setGame(shuffledGame)
+  }, [])
+
+  useEffect(() => {
+    const finished = !game.some(card => !card.flipped);
+    if (finished && game.length > 0) {
+      setTimeout(() => {
+        setIsModalVisible(true);
+      }, 500)
+    }
+  }, [game]);
+
+  if (indexesOfFlippedCards.length === 2) {
+    const match = game[indexesOfFlippedCards[0]].fontId === game[indexesOfFlippedCards[1]].fontId;
+
+    if (match) {
+      const newGame = [...game];
+      newGame[indexesOfFlippedCards[0]].flipped = true;
+      newGame[indexesOfFlippedCards[1]].flipped = true;
+      setGame(newGame);
+
+      const newIndexes = [...indexesOfFlippedCards];
+      newIndexes.push(false);
+      setIndexesOfFlippedCards(newIndexes);
+    } else {
+      const newIndexes = [...indexesOfFlippedCards];
+      newIndexes.push(true);
+      setIndexesOfFlippedCards(newIndexes);
+    }
+  }
+
+  return (
+    <div className="cards-section">
+      {game.map((card, index) => (
+        <div key={index}>
+          <Card
+            id={index}
+            game={game}
+            flipCount={flipCount}
+            setFlipCount={setFlipCount}
+            indexesOfFlippedCards={indexesOfFlippedCards}
+            setIndexesOfFlippedCards={setIndexesOfFlippedCards}
+            letterToBeDisplayed={alwaysDifferentLetter ? letters[index] : letterToBeDisplayed}
+            font={card.font}
+            showFontInfo={showFontInfo}
+            backgroundColor={backgroundColor}
+          />
+          <Modal title="Well Done!"
+            visible={isModalVisible}
+            onOk={handleStartOver}
+            onCancel={() => setIsModalVisible(false)}>
+            Would you like to play again?
+          </Modal>
+        </div>
+      ))}
+    </div>
+  )
+
+}
+
+export default Game;
---
+++ pages/components/TitleBar.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link';
+
+export default function TitleBar() {
+  return (
+    <div className="title-section">
+      <h1>TypoMemory</h1>
+      <p>a font memory game</p>
+      <div className="credits-section">
+        <Link href="/credits/thank-you">
+          <a>credits</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
